e7402a202dec0763dba0014e0cf64d09f6546e5f
Pranav <Pranav@DESKTOP-KB5NO3L>
2024-11-08 14:25:04 +0530

    second commit

--- Java/src/logical/Sum_of_Array.java
+++ Java/src/logical/Sum_of_Array.java
@@ -3,8 +3,14 @@ package logical;
 public class Sum_of_Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int arr[]= {10, 20, 30, 40, 50};
+		int sum=0;
+
+		for(int i=0;i<=arr.length-1;i++) {
+			sum+=arr[i];
+		}
+		System.out.println("Sum of Array: "+sum);
 	}
 
 }
